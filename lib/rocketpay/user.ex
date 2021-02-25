82335dc06538ff6a3fae7c4740d15edbd9bd45cb
tiago.bani <tiago.bani@yahoo.com.br>
2021-02-24 23:05:53 -0300

    [feat]: create accounts

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -3,6 +3,7 @@ defmodule Rocketpay.User do
   import Ecto.Changeset
 
   alias Ecto.Changeset
+  alias Rocketpay.Account
 
   @required_params [:name, :age, :email, :password, :nickname]
 
@@ -15,6 +16,7 @@ defmodule Rocketpay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+    has_one :account, Account
 
     timestamps()
   end
